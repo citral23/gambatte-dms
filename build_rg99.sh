9768bc7a15a6ad8e4e8a53d4dd142902e8ee9b6d
Christophe Branchereau <cbranchereau@gmail.com>
2022-10-18 11:48:17 +0200

    Add lepus and rg99 build script + multi opk packager
    Signed-off-by: Christophe Branchereau <cbranchereau@gmail.com>

--- build_rg99.sh
+++ build_rg99.sh
@@ -4,10 +4,10 @@ BDAT=$(date +"%Y%m%d-%H%M%S")
 echo '#define BUILDDATE "'$BDAT'"' >./gambatte_sdl/builddate.h
 
 echo "cd libgambatte && scons"
-(cd libgambatte && scons -Q target=gcw0) || exit
+(cd libgambatte && scons -Q target=rg99) || exit
 echo "cd gambatte_sdl && scons"
-(cd gambatte_sdl && scons -Q target=gcw0)
-mv gambatte_sdl/gambatte_sdl gambatte_sdl/gambatte-dms.gcw0
+(cd gambatte_sdl && scons -Q target=rg99)
+mv gambatte_sdl/gambatte_sdl gambatte_sdl/gambatte-dms.rg99
 
 echo "cd gambatte_sdl && scons -c"
 (cd gambatte_sdl && scons -c)
@@ -20,7 +20,7 @@ rm -rf *gambatte*/.scon*
 find . -type f -iname \*.o -delete
 find . -type f -iname gambatte_sdl -delete
 
-rm -f gambatte-dms-gcw0-r572u4-$BDAT.opk
-#mksquashfs ./dist/gcw0/default.gcw0.desktop ./gambatte_sdl/gambatte-dms.gcw0 ./dist/gambatte_dms.png ./dist/manual.txt gambatte-dms-gcw0-r572u4-$BDAT.opk -all-root -no-xattrs -noappend -no-exports
+rm -f gambatte-dms-rg99-r572u4-$BDAT.opk
+#mksquashfs ./dist/rg99/default.rg99.desktop ./gambatte_sdl/gambatte-dms.rg99 ./dist/gambatte_dms.png ./dist/manual.txt gambatte-dms-rg99-r572u4-$BDAT.opk -all-root -no-xattrs -noappend -no-exports
 
-#find . -type f -iname gambatte-dms.gcw0 -delete
+#find . -type f -iname gambatte-dms.rg99 -delete
